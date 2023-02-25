2e54578e4d2deb468b958749c99ed84a00440037
Alberth G. Ruado <alberthruado@gmail.com>
2023-02-26 02:07:35 +0800

    Adding range and price javascript functionality

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const rangePrice = document.getElementById("range-price");
+const views = document.getElementById("views");
+const price = document.getElementById("price");
+
+price.innerText = `$${100}.00`;
+views.innerText = `${10}K`;
